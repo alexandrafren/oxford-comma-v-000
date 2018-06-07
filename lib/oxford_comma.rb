3994942153bd0b395aeb671f804e41e35948e32b
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-07 14:07:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
    return array.join
  elsif array.length == 2
    holder_array = array.pop
-   array.push("and")
+   array.push("and ")
    array.push(holder_array)
    array.join(" ")
  elsif array.length > 2
