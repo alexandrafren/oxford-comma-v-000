68ea2da0fe7e89fef31fe15c4451cb12907179bc
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-07 14:04:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
  if array.length <= 1
    return array.join
- elsif array.length = 2
+ elsif array.length == 2
    holder_array = array.pop
    array.push("and")
    array.push(holder_array)
