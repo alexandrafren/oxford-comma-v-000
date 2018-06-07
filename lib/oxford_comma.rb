d08aa9f72ea8f6ce5a1f20a9622ba7a74c8e3d08
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-07 14:07:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,12 +3,12 @@ def oxford_comma(array)
    return array.join
  elsif array.length == 2
    holder_array = array.pop
-   array.push("and ")
+   array.push("and")
    array.push(holder_array)
    array.join(" ")
  elsif array.length > 2
    holder_array = array.pop
-   array.push("and")
+   array.push("and ")
    oxford_comma = array.join(", ")
    oxford_comma << holder_array
  else
